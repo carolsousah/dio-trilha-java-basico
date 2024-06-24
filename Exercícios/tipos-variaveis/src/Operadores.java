54414f2769f263bd21e4d000e5e3ea436634a528
Carol Sousa <159448667+carolsousah@users.noreply.github.com>
2024-06-24 17:13:01 -0300

    Exercícios

---
+++ Exercícios/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        boolean variavel = false;
+
+        variavel = !variavel;
+        System.out.println(variavel);
+    }
+}  
+
+
+
