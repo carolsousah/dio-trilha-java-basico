2b0f67085353b40badee02e3f9adff0697fb2b32
Carol Sousa <159448667+carolsousah@users.noreply.github.com>
2024-07-09 18:31:24 -0300

    Lab Project - Publicando Sua API REST na Nuvem

---
+++ Santander-2024-Backend-com-Java/src/main/java/me.dio/Application.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_2024_Backend_com_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santander2024BackendComJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santander2024BackendComJavaApplication.class, args);
+	}
+
+}
---
+++ Santander-2024-Backend-com-Java/src/test/java/me/dio/Santander_2024_Backend_com_Java/Santander2024BackendComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_2024_Backend_com_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santander2024BackendComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
